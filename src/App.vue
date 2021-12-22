b491340228e17649ab60fab0f6a5b717cf813db3
abusizhishen <abusizhishen@gmail.com>
2021-12-23 00:53:09 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <div class="mt-5 container">
+      <div class="row justify-content-center border py-5">
+        <div class="col-5">
+          <h4 class="mb-3">Draggable 1</h4>
+          <draggable class="draggable-list" :list="list1" group="my-group">
+            <div class="list-item" v-for="element in list1" :key="element.name">
+              {{ element.name }}
+            </div>
+          </draggable>
+        </div>
+
+        <div class="col-5">
+          <h4 class="mb-3">Draggable 2</h4>
+          <draggable class="draggable-list" :list="list2" group="my-group">
+            <div class="list-item" v-for="element in list2" :key="element.name">
+              {{ element.name }}
+            </div>
+          </draggable>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import draggable from "vuedraggable";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    draggable,
+  },
+  data() {
+    return {
+      list1: [{ name: "Drag Me!" }],
+      list2: [{ name: "Drag Me Too!" }],
+    };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.draggable-list {
+  background: #3f51b5;
+  color: #fff;
+  border: 1px solid;
+  height: 50vh;
+}
+.list-item {
+  margin: 10px;
+  padding: 40px;
+  cursor: pointer;
+  font-size: 18px;
+  border-radius: 5px;
+  background: #f44336;
+  display: inline-block;
 }
 </style>
